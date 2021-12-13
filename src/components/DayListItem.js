6c1d1bec3f738b6a1e57c92fd8360aa735d22536
Khushbakht Zehra Naqvi <khushi.zehra@gmail.com>
2021-12-13 10:37:00 -0500

    initial commit

---
+++ src/components/DayListItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+export default function DayListItem(props) {
+  const selectStyle = classNames("day-list__item",{
+    "day-list__item--full": (props.spots === 0),
+    "day-list__item--selected": props.selected,
+});
+  return (
+    <li className={selectStyle} onClick={() => props.setDay(props.name)} selected={props.selected}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots(props)}</h3>
+    </li>
+  );
+}
+
+const formatSpots = (props) => {
+const noSpots = "no spots remaining";
+const spots = `${props.spots} spots remaining`;
+const oneSpot = "1 spot remaining";
+if (props.spots === 0) {
+  return noSpots;
+}
+if (props.spots === 1) {
+  return oneSpot;
+} 
+  return spots;
+}
